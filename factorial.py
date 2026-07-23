658153a998d697dda9f806b9f9b1956d846b6cc2
Brama-G <bramarambika.g2024@vitstudent.ac.in>
2026-07-23 14:44:10 +0530

    Updated factorial.py to remove input

--- factorial.py
+++ factorial.py
@@ -1,7 +1,6 @@
 # Python program to find factorial of a number
-num = input("Enter a number: ")
+num = 6
 fac = 1
-num = (int) (num)
 for i in range(1, num+1):
   fac = fac*i
 print(f"Factorial is: {fac}")
